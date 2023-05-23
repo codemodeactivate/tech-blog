972abce8c773bbd6d2915ef655de4423a6b6b4bf
codemodeactivate <jeff.zenko@gmail.com>
2023-05-22 22:57:38 -0400

    structure stuff

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Role = require('./Role');
+const Category = require('./Category');
+const Tag = require('./Tag');
+module.exports = {
+    User,
+    Post,
+    Comment,
+    Role,
+    Category,
+    Tag
+ };
