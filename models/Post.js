dcd8ff70199a2185569a8c3a17bb01dae1160aad
codemodeactivate <jeff.zenko@gmail.com>
2023-05-23 11:22:58 -0400

    Models v1.0

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: Datatypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
