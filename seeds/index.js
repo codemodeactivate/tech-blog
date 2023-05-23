6a45d02ad7429c69593c458aedf95b97bc4b5578
codemodeactivate <jeff.zenko@gmail.com>
2023-05-23 19:35:47 -0400

    post seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,17 @@
 // seeds/index.js
 const seedUsers = require('./userSeeds');
-//const seedPosts = require('./postSeeds');
+const seedPosts = require('./postSeeds');
 const seedTags = require('./tagSeeds');
 
 const sequelize = require('../config/connection');
+
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUsers();
   await seedTags();
+  await seedPosts();
   //await seedPosts();
   // ...other seed functions...
 
---
+++ seeds/postSeeds.js
@@ -0,0 +1,18 @@
+const { Post } = require("../models");
+const faker = require("faker");
+
+const seedPosts = async (numPosts = 10) => {
+    const postData = [];
+
+    for (let i = 0; i < numPosts; i++) {
+        postData.push({
+            title: faker.lorem.sentence(),
+            post_content: faker.lorem.paragraph(),
+            user_id: Math.floor(Math.random() * 10) + 1,
+            post_url: faker.internet.url(),
+        });
+    }
+
+    await Post.bulkCreate(postData);
+};
+module.exports = seedPosts;
